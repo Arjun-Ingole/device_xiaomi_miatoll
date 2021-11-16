65c8136a6e7635c81f7d592208eaee3bab6be54f
Arjun-Ingole <arjuningole38@gmail.com>
2021-11-16 18:27:42 +0530

    miatoll: Remove current lights hal implimentation

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,9 +99,6 @@ BOARD_RAMDISK_OFFSET := 0x01000000
 
 TARGET_KERNEL_CONFIG := vendor/miatoll-perf_defconfig
 
-# Lights
-TARGET_PROVIDES_LIBLIGHT := true
-
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 
