b21e09ca84372f8eb62b3485c59ef88d04cbcddd
Arjun-Ingole <arjun@project404.us>
2021-11-17 23:16:23 +0530

    miatoll: Provide Perf hal on vendor
    Change-Id: I7a2af9398b2b0037b8bca6eb4e10c00b9897aad0
    Signed-off-by: Arjun-Ingole <arjun@project404.us>

--- miatoll.mk
+++ miatoll.mk
@@ -176,6 +176,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Perf
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@2.2.vendor
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
     $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
